b8e8c9defa9962a18bea1b5b0514ba2af3779431
Yucky <registyucky@gmail.com>
2021-06-23 01:19:53 +0900

    disable nortification

--- input_form.py
+++ input_form.py
@@ -2,6 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome()
+chrome_options= webdriver.ChormeOptions()
+chrome_options.add_argument('disable-notifications')
+
 driver.get("https://ytmp3.cc/youtube-to-mp3/")#put here the adress of your page
 # elem = driver.find_elements_by_xpath("//*[@type='submit']")#put here the content you have put in Notepad, ie the XPath
 input_field = driver.find_element_by_id('input')
